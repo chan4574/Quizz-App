c53899ab348c0f3a84a9b3b9ba93463ddedca4cf
chan4574 <sigma292470@gmail.com>
2026-08-16 16:55:05 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quiz-maker-v2';
+const CACHE_NAME = 'quiz-maker-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
